a8799af2980272d33ac18877850e4c39ab7b2a1e
Mahdi89 <jelodari.m@gmail.com>
2018-12-15 13:04:44 +0000

    Update scripts.

--- bin/bench.sh
+++ bin/bench.sh
@@ -3,13 +3,13 @@
 # set Pregel_Project_root env variable
 # to DIR where the project is cloned to
  
-root=$Pregel_Project_root
+root=$Project_root
 conf_file="config.go"
 out_file="bench.out"
 
 # TODO allow iteration over 
 # SUPER_STEPS and CONN_DEGREE too
-cd $root && rm -rf $out_file $conf_file
+cd $root/pagerank && rm -rf $out_file $conf_file
 for value in $(seq 1 $1)
 do
 
@@ -19,7 +19,7 @@ do
 	// Set high-level meta parameters
 	const SUPER_STEPS = 100
 	const CONN_DEGREE = $2
-	const NUM_VERTEX = $value * 5
+	const NUM_VERTEX = $(($value * 5))
 	EOM
 
 	go test -bench=. -benchmem >> $out_file
